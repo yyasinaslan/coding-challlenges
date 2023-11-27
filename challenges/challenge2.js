a77c938600816f9391f4544fa21434f5dcba2467
YSN <yyasin.aslan@gmail.com>
2023-11-27 20:45:13 +0300

    challenge 2

--- challenges/challenge2.js
+++ challenges/challenge2.js
@@ -1,5 +1,24 @@
 /*
 The weight of an array is defined as the length of the most frequently occurring integer in the array.
 
+Create a function that returns the length of the smallest subarray, with consecutive elements, 
+from a given array, such that the weight of subarray is equal to weight of the given array.
 
 */
+
+/**
+ * @param {number[]} k
+ */
+function subarrayLegnth(k) {
+  // Your code here
+}
+
+// Test cases
+console.log(subarrayLegnth([2, 4, 6, 8, 2, 6, 8, 8]));
+// Should return { weight: 3, subarraySize: 5 }
+
+console.log(subarrayLegnth([1, 2, 2, 4, 1]));
+// Should return { weight: 2, subarraySize: 2 }
+
+console.log(subarrayLegnth([5, 10, 15, 20, 5, 10]));
+// Should return { weight: 2, subarraySize: 5 }
