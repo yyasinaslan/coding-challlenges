3abf6d872a3f7b99f733eb87a3da3cb7623833e6
YSN <yyasin.aslan@gmail.com>
2023-11-27 23:57:40 +0300

    challenge 3

---
+++ challenges/challenge3.js
@@ -0,0 +1,20 @@
+/*
+Challenge: Unique Pairs
+Write a function that takes an array of integers and a target sum. 
+The function should find and return all unique pairs of integers from the 
+array that add up to the target sum. Each pair should be represented as an array with two elements.
+
+For example, given the array [3, 1, 4, 2, 2] and a target sum of 5, 
+the function should return [[3, 2], [1, 4]] because both pairs (3, 2) and (1, 4) add up to 5.
+- The order of pairs doesn't matter.  If a pair (a, b) is included, (b, a) should not be included.
+*/
+
+
+function findUniquePairs(arr, targetSum) {
+    // Your code here
+    
+  }
+  
+  // Test case
+  console.log(findUniquePairs([3, 1, 4, 2, 2], 5));
+  // Should return [[3, 2], [1, 4]]
